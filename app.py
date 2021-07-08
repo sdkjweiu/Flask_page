6fd195c94651993e423c2d4a8efafc6aa4d2509f
sdkjweiu <sdkjweiu@gmail.com>
2021-07-08 19:44:51 +0900

    4일차 글 수정 기능 추가

--- app.py
+++ app.py
@@ -72,32 +72,53 @@ def body_edit():
         return redirect('/')
     return render_template('body_edit.html')
 
+@app.route('/body_update/<po_st>', methods=['GET','POST'])
+def body_update(po_st):
+    if session.get('user') == session.get('p.user_id'): # 검증, 작성자랑 수정하려는 사람이 같은지
+        conn = pymysql.connect(host='192.168.182.128', port=3306, user=pymysql_con.user, passwd=pymysql_con.passwd, database='page')
+        cur = conn.cursor() 
+        if request.method == 'POST':        # submit을 눌렀을 때 작동하는 함수
+            cur.execute('update post set title="%s", body="%s" where post_id = "%s"' %(request.form.get('udt_title'), request.form.get('udt_body'),po_st))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect('/body/'+po_st)
+
+        cur.execute('select title, body from post where post_id = "%s" ' %(po_st))      # submit을 안 눌렀을때, 수정버튼 클릭 전
+        p_post = cur.fetchone()
+        cur.close()
+        conn.close()
+        
+    return render_template('body_update.html', p_post=p_post, po_st=po_st)
+
+
 
 @app.route('/body/<text>')
 def body(text):
     conn = pymysql.connect(host='192.168.182.128', port=3306, user=pymysql_con.user, passwd=pymysql_con.passwd, database='page')
     cur = conn.cursor()
-    cur.execute('select p.title, p.body, p.stampdate, u.nickname, p.post_id from post p, users u where p.user_id = u.user_id and p.post_id="%s"  ' %(text))
+    cur.execute('select p.title, p.body, p.stampdate, u.nickname, p.post_id, p.user_id from post p, users u where p.user_id = u.user_id and p.post_id="%s"  ' %(text)) #게시물 보여주기용으로 데이터 조회
     body=cur.fetchone()
-    cur.execute('select u.nickname, c.contents, c.stampdate from comment c, users u where u.user_id = c.user_id and c.post_id = "%s"' %(body[4]))
+    session['p.user_id'] = body[5]
+    cur.execute('select u.nickname, c.contents, c.stampdate from comment c, users u where u.user_id = c.user_id and c.post_id = "%s"' %(body[4])) #어느 게시물의 해당하는 댓글인지 확인하기 위해서 아까 body[4](post_id)를 이용
     comment=cur.fetchall()
     cur.close()
     conn.close()
     return render_template('body.html', body=body, ment=comment, text=text)
 
-@app.route('/ment', methods=['POST'])
-def ment():
+@app.route('/ment/<abc>', methods=['POST'])
+def ment(abc):
     if session.get('user') == None:
         flash("로그인을 해야합니다")
         return redirect('/')
     if request.method == 'POST':
         conn = pymysql.connect(host='192.168.182.128', port=3306, user=pymysql_con.user, passwd=pymysql_con.passwd, database='page')
         cur = conn.cursor()
-        cur.execute('insert into comment (user_id, contents, stampdate, post_id) values ("%s","%s","%s","%s")' %(session.get('user'),request.form.get('co_ment'), datetime.now().strftime('%Y-%m-%d %H:%M'), request.form.get('hide_b')))
+        cur.execute('insert into comment (user_id, contents, stampdate, post_id) values ("%s","%s","%s","%s")' %(session.get('user'),request.form.get('co_ment'), datetime.now().strftime('%Y-%m-%d %H:%M'), abc))
         conn.commit()
         cur.close()
         conn.close()
-        return redirect('/body/'+request.form.get('hide_b'))
+        return redirect('/body/'+abc)
 
 
 if __name__ == '__main__':      # 파이썬이 이 파일을 인터프리팅할때 해당 조건문의 실행문을 실행시키는(True) 조건 내용, 이 파일을 직접 불러올때만 True로 바뀌고 그 밑에 코드를 실행,
